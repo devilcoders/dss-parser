d1d383ed18e08aae7f7e52ec865e9b27129c9f9d
Graham Hadgraft <graham.hadgraft@stylefoundry.co.uk>
2015-06-05 16:13:52 +0100

    Parse css and scss for blocks of dss comments

--- lib/dss_parser.rb
+++ lib/dss_parser.rb
@@ -1,7 +1,75 @@
 require "dss_parser/version"
 
 class DssParser
-  def initialize
+  def initialize(stylesheet_path)
+    @stylesheet_path = stylesheet_path
+  end
 
+  def find_css_files
+    Dir.glob("#{@stylesheet_path}**/*.css*")
+  end
+
+  def get_comments
+    comments = []
+
+    find_css_files.each do |file_path|
+      css_file = IO.read(file_path)
+      comments |= parse_for_comments(css_file)
+    end
+
+    comments
+  end
+
+  def parse_for_comments(css)
+    comment_blocks = []
+    in_comment=false
+    current_comment = []
+    lines = css.split "\n"
+
+    lines.each do |line_of_css|
+      line_of_css.strip!
+
+      puts ""
+      puts line_of_css
+
+      if commented_line?(line_of_css, in_comment)
+        puts "true"
+        in_comment = true
+        current_comment.push "#{line_of_css}"
+        puts line_of_css.size
+        if line_of_css.size == 0 || line_of_css.end_with?('*/')
+          in_comment = false
+          puts 'end of comments'
+        end
+      else
+        puts "false"
+        in_comment = false
+        comment_blocks.push current_comment if current_comment.size > 0
+        current_comment = []
+      end
+    end
+
+    puts comment_blocks.inspect
+
+  comment_blocks
+    # Remove comments which do not fit DSS syntax
+    #comment_blocks.select { |comment| comment.any? { |s| s.downcase.include? "@name" } }
+  end
+
+  private
+
+  def commented_line?(line, in_comment)
+    if css_comment?(line) || scss_comment?(line) || in_comment == true
+      in_comment = true
+      return true
+    end
+  end
+
+  def css_comment?(line)
+    line.start_with?('/*','*/') && !line.end_with?('*/')
+  end
+
+  def scss_comment?(line)
+    line.start_with? '//'
   end
 end
--- spec/lib/dss_parser_spec.rb
+++ spec/lib/dss_parser_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe DssParser do
-  pending "write it"
+  describe '#find_css_files' do
+    it 'finds all css/scss files in the folder  passed into the parser' do
+      parser = DssParser.new("#{File.expand_path('../..', __FILE__)}/fixtures/")
+      css_files = parser.find_css_files
+
+      expect(css_files).to be_instance_of Array
+      expect(css_files.size).to eq 2 
+    end
+  end
+
+  describe '#get_comments' do
+    it 'finds comments in css files' do
+      parser = DssParser.new("#{File.expand_path('../..', __FILE__)}/fixtures/")
+      comments = parser.get_comments
+
+      expect(comments.size).to eq 1
+    end
+  end
 end
